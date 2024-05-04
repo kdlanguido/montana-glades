e519a2a06044659d1d5f1719ba4f52043427626e
kd <alyastubigman@gmail.com>
2024-05-05 06:54:01 +0800

    adjusted svgs for deployment

--- src/components/constructionUpdates/constructionUpdatesModal.php
+++ src/components/constructionUpdates/constructionUpdatesModal.php
@@ -3,7 +3,11 @@
         <div class="modal-content">
             <div class="modal-header border-bottom-0 p-4 p-lg-5 pb-2 pb-lg-2 position-relative">
 
-                <button class="btn position-absolute border rounded" data-bs-dismiss="modal" style="right:10px; top:10px;"><i class="fa-solid fa-x"></i></button>
+                <button class="btn position-absolute border rounded text-light" data-bs-dismiss="modal" style="right:10px; top:10px; z-index:100;">
+                    <svg fill="#00000cc" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                    </svg>
+                </button>
 
                 <div class="container-fluid">
                     <div class="row">
@@ -18,13 +22,19 @@
                                 <small class="d-none d-lg-block" style="color: rgba(88, 99, 126, 0.6);">Share</small>
                                 <div class="d-flex mt-2">
                                     <button class="nav-tab-buttons no-tab nav-link me-1 p-0 toolbar-btn">
-                                        <i class="fa-brands fa-facebook"></i>
+                                        <svg fill="#3678ac" height="17" width="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                            <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
+                                        </svg>
                                     </button>
                                     <button class="nav-tab-buttons no-tab  nav-link mx-1 p-0 toolbar-btn">
-                                        <i class="fa-solid fa-envelope"></i>
+                                        <svg fill="#3678ac" height="17" width="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                                        </svg>
                                     </button>
                                     <button class="nav-tab-buttons no-tab  nav-link mx-1 p-0 toolbar-btn">
-                                        <i class="fa-solid fa-link"></i>
+                                        <svg fill="#3678ac" height="17" width="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                            <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                                        </svg>
                                     </button>
                                 </div>
                             </div>

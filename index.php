ab62e1575f8776be659efbba763cc9ed16696434
kd <alyastubigman@gmail.com>
2024-05-05 05:42:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,9 @@
     <link rel="preconnect" href="https://cdnjs.cloudflare.com">
     <link rel="preconnect" href="https://code.jquery.com">
     <link rel="preconnect" href="https://res.cloudinary.com">
-    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- <link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/dlobngrjy/image/upload/v1714694908/global/logowithbokeh_white_riwhix_ogw01l.webp"> -->
+    <link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/dlobngrjy/image/upload/v1714694908/global/logowithbokeh_white_riwhix_ogw01l.webp">
     <link rel="stylesheet" href="src/libs/splide.min.css">
     <link rel="stylesheet" href="src/libs/splide-sea-green.min.css">
     <link rel="stylesheet" href="src/libs/bootstrap.css">

d384e6b9ed763681f5eeb804e6e81bd4285bde60
kd <alyastubigman@gmail.com>
2024-05-05 06:46:49 +0800

    adjusted settings for deployment

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
 
     <title>Montana Glades</title>
 
-    <link rel="preconnect" href="https://cdnjs.cloudflare.com">
-    <link rel="preconnect" href="https://code.jquery.com">
+    <!-- <link rel="preconnect" href="https://cdnjs.cloudflare.com"> -->
+    <!-- <link rel="preconnect" href="https://code.jquery.com"> -->
     <link rel="preconnect" href="https://res.cloudinary.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="icon" type="image/x-icon" href="src/static/logo.webp">
     <link rel="stylesheet" href="src/styles/all.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" integrity="sha512-dPXYcDub/aeb08c63jRq/k6GaKccl256JQy/AnOq7CAnEZ9FzSL9wSbcZkMp4R26vBsMLFYH4kQ67/bbV8XaCQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    -->
     <!-- <link rel="stylesheet" href="src/libs/splide.min.css"> -->
     <!-- <link rel="stylesheet" href="src/libs/splide-sea-green.min.css"> -->
     <!-- <link rel="stylesheet" href="src/libs/bootstrap.css"> -->
--- src/components/widgetButtons/widgetButtons.php
+++ src/components/widgetButtons/widgetButtons.php
@@ -1,9 +1,15 @@
 <button class="overview-side-buttons btn position-absolute end-0 z-3" data-bs-toggle="modal" data-bs-target="#modal-share" id="btnSidebuttonShare">
-    <i class="bi bi-share"></i>
+    <!-- <i class="bi bi-share"></i> -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+        <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+    </svg>
     <span class="ms-2">Share</span>
 </button>
 
 <button class="overview-side-buttons btn position-absolute end-0 z-3 btnSidebuttonFullscreen" id="btnSidebuttonFullscreen">
-    <i class="bi bi-arrows-angle-expand"></i>
+    <!-- <i class="bi bi-arrows-angle-expand"></i> -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707" />
+    </svg>
     <span class="ms-2">Fullscreen</span>
 </button>

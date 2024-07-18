61a6785a8d60dbbe3c341c2d85f37ee928232307
kd <alyastubigman@gmail.com>
2024-07-18 14:15:26 +0800

    Update inquireNowModal.php

--- src/components/inquireNow/inquireNowModal.php
+++ src/components/inquireNow/inquireNowModal.php
@@ -1,13 +1,21 @@
 <div class="modal fade" id="modal-inquire-now" tabindex="-1" aria-labelledby="share_modal" aria-hidden="true">
     <div class="modal-dialog modal-fullscreen-lg-down modal-xl">
         <div class="modal-content">
-            <div class="modal-body p-0">
+            <div class="modal-body p-0 position-relative">
+
+                <button class="btn position-absolute border rounded text-light d-md-none" data-bs-dismiss="modal" style="right:10px; top:10px; z-index:100;">
+                    <svg fill="#00000cc" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"></path>
+                    </svg>
+                </button>
+
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col p-5 position-relative">
 
-                            <div class="tab-header-1 mb-3">
+                            <div class="tab-header-1 mb-3 d-flex justify-content-between align-items-start">
                                 <h5>Contact Us</h5>
+
                             </div>
 
                             <div class="container-fluid ps-0 ms-0">
@@ -109,7 +117,7 @@
                                     <p>(Globe) 0926-047-5858;<br>(Smart)0968-391-4533</p>
                                 </section>
 
-                                <img src="src/static/logo.webp" alt="">
+                                <img src="src/static/logo.webp" alt="" class="d-none d-md-block">
                             </div>
                         </div>
                     </div>

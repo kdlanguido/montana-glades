126f6e0ff005d6e0bca9c0781d9cfb59cf6801d1
Kd <86334894+kdlanguido@users.noreply.github.com>
2025-05-19 23:07:43 +0800

    update site plan

--- src/components/navTabs/tabPlans.php
+++ src/components/navTabs/tabPlans.php
@@ -1,3 +1,3 @@
 <div class="tab-pane fade p-0 m-0 position-relative w-100 text-center p-3" id="tabPlans" role="tabpanel">
-    <img style="height:auto;" id="siteplanImg" crossorigin="anonymous" referrerpolicy="no-referrer" src="src/static/SITEPLAN.webp" alt="siteplan image">
+    <img style="height:auto;" id="siteplanImg" crossorigin="anonymous" referrerpolicy="no-referrer" src="src/static/salesmap-42025.webp" alt="siteplan image">
 </div>
